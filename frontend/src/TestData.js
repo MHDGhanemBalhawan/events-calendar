f33cbdef1bee9248c2858af478aa6679aa4abe10
Daleel Hagy <daleel.n.hagy@gmail.com>
2018-10-05 00:30:21 +0100

    Creating React App

---
+++ frontend/src/TestData.js
@@ -0,0 +1,20 @@
+const testData = [
+    {
+        name: "CSS Class",
+        description:
+            "Class work: introduction to CSS features, margins, grid, and basic CSS ",
+        date: "Date: 16/10/2018"
+    },
+    {
+        name: "JavaScript",
+        description: "Class work: varialbles, Arrays, and Objects",
+        date: "Date: 20/10/2018"
+    },
+    {
+        name: "React",
+        description:
+            "Class work: introducing components, Creating Funtions and Classes, and props",
+        date: "Date: 23/10/2018"
+    }
+];
+export {testData};
