6a0a895930de18b959675f62734c948df7991a09
Behnaz <behnaz.pourafshari@gmail.com>
2018-10-10 22:09:36 +0100

    json event is made with some changes to route

--- backend/route/router.js
+++ backend/route/router.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const router = express.Router();
-
+const events = require('../events.json')
 router.get("/home", (req, res) => {
     res.send("it is working");
 })
 
+router.get("/all-events", (req, res) => {
+    res.json(events);
+})
 
+router.get("/all-events/:id", (req, res) => {
+
+    const id = req.params.id;
+    let result = events.filter(item => item.id === id)
+    res.json(result)
+})
 
 
 
--- backend/server.js
+++ backend/server.js
@@ -9,5 +9,5 @@ const router = require("./route/router")
 
 app.use("/", router)
 app.listen(process.env.PORT || 3001, function () {
-    console.log("Server is listening on port 3000. Ready to accept requests!");
+    console.log("Server is listening on port 3001. Ready to accept requests!");
 });
