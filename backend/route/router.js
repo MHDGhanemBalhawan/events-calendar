d4dcbab94b0d0b098a73c2fe9b958c0af52c2481
Steve <mhdghanembalhawan@hotmail.com>
2018-10-12 12:26:08 +0100

    Steve add the data folder and the database settings in the json file.

--- backend/route/router.js
+++ backend/route/router.js
@@ -1,12 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get("/home", (req, res) => {
-    res.send("it is working");
-})
-
-
-
-
+router.get("/", (req, res) => {
+  res.send("it is working");
+});
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
-const router = require("./route/router")
+const router = require("./route/router");
 
+const mogo = require("mongodb");
 
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/event_calender");
 
+var db = mongoose.connection;
 
+db.on("error", console.error.bind(console, "connection error:"));
 
+db.once("open", function() {
+  console.log("Database Connection Successful!");
+});
 
-app.use("/", router)
-app.listen(process.env.PORT || 3001, function () {
-    console.log("Server is listening on port 3000. Ready to accept requests!");
+app.use("/", router);
+app.listen(process.env.PORT || 3001, function() {
+  console.log("Server is listening on port 3001. Ready to accept requests!");
 });
