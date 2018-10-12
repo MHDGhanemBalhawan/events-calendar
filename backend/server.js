cc2367ccab9aced59765d5ec76d36497dba317bb
Daleel Hagy <daleel.n.hagy@gmail.com>
2018-10-12 20:19:40 +0100

    fetch data and page for floater to register

--- backend/server.js
+++ backend/server.js
@@ -2,11 +2,7 @@ const express = require("express");
 const app = express();
 const router = require("./route/router");
 
-<<<<<<< HEAD
-app.use("/", router);
-app.listen(process.env.PORT || 3001, function() {
-  console.log("Server is listening on port 3001. Ready to accept requests!");
-=======
+
 
 
 
@@ -15,6 +11,5 @@ app.listen(process.env.PORT || 3001, function() {
 app.use("/", router)
 app.listen(process.env.PORT || 3001, function () {
     console.log("Server is listening on port 3001. Ready to accept requests!");
->>>>>>> 37f16359ddb1026c07767b6bd6ac8a479ce30fec
 });
 
