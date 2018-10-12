882330fe760e52a7f8582f3a5a2eaf3fcf747086
Steve <mhdghanembalhawan@hotmail.com>
2018-10-12 19:00:29 +0100

    work in progress

--- backend/route/router.js
+++ backend/route/router.js
@@ -1,25 +1,43 @@
 const express = require("express");
 const router = express.Router();
+const events = require("../data/events");
+
+// router.get("/", (req, res) => {
+//   res.send("it is working");
+// });
 
 router.get("/", (req, res) => {
-  res.send("it is working");
-});
-
-module.exports = router;
-
-const events = require("../data/events.json");
-router.get("/home", (req, res) => {
-  res.send("it is working");
-});
-
-router.get("/all-events", (req, res) => {
   res.json(events);
 });
 
-router.get("/all-events/:id", (req, res) => {
+router.get("/:id", (req, res) => {
   const id = req.params.id;
   let result = events.filter(item => item.id === id);
   res.json(result);
 });
+
+router.post("/", (req, res) => {
+  console.log("Handeling post request");
+  res.end();
+});
+
+router.put("/", (req, res) => {
+  console.log("Handeling post request");
+  res.end();
+});
+
+router.delete("/", (req, res) => {
+  console.log("Handeling post request");
+  res.end();
+});
+
+router.get("/", (req, res) => {
+  res.json(events);
+});
+// router.get(`${EVENTS_BASE_URL}/:id`, (req, res) => {
+//   const id = req.params.id;
+//   let result = events.filter(item => item.id === id);
+//   res.json(result);
+// });
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const app = express();
 const router = require("./route/router");
+const events = require("./data/events.json");
+const lodash = require("lodash");
 
-const mogo = require("mongodb");
+const buildurl = (version, path) => `/api/${version}/${path}`;
 
-const mongoose = require("mongoose");
+const EVENTS_BASE_URL = buildurl("v1", "events");
+// const mogo = require("mongodb");
 
-mongoose.connect("mongodb://localhost:27017/event_calender");
+// const mongoose = require("mongoose");
 
-var db = mongoose.connection;
+// mongoose.connect("mongodb://localhost:27017/event_calender");
 
-db.on("error", console.error.bind(console, "connection error:"));
+// var db = mongoose.connection;
 
-db.once("open", function() {
-  console.log("Database Connection Successful!");
-});
+// db.on("error", console.error.bind(console, "connection error:"));
+
+// db.once("open", function() {
+//   console.log("Database Connection Successful!");
+// });
+
+app.use(EVENTS_BASE_URL, router);
 
 app.use("/", router);
 app.listen(process.env.PORT || 3001, function() {
