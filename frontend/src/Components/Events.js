1bbe2702d582a8505dca0421b2a733170725386e
Daleel Hagy <daleel.n.hagy@gmail.com>
2018-10-14 15:59:05 +0100

    editing link in fetch

--- frontend/src/Components/Events.js
+++ frontend/src/Components/Events.js
@@ -5,7 +5,7 @@ import { Event } from "./Event.js";
 const Events = props => {
     return (
         <div class="events">
-            {fetch("http://localhost:3001//events")
+            {fetch("http://localhost:3001/events")
                 .then(res => res.json())
                 .then(testData => {
                     testData.map(function(event) {
