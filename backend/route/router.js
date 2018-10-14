85c555e3bb03fc7f413d2c465c04ac8aa585a8e5
Daleel Hagy <daleel.n.hagy@gmail.com>
2018-10-14 15:51:14 +0100

    adding line space on  router.js

--- backend/route/router.js
+++ backend/route/router.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const events = require("../events.json");
+
 router.get("/home", (req, res) => {
   res.send("it is working");
 });
+
 router.get("/all-events", (req, res) => {
   res.json(events);
 });
