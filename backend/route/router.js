1102d417dd488ca7d20bbda4f671320dacef8ad3
Daleel Hagy <daleel.n.hagy@gmail.com>
2018-10-21 22:03:08 +0100

    removing unnecessary app object

--- backend/route/router.js
+++ backend/route/router.js
@@ -1,8 +1,6 @@
 const express = require("express");
-const app = express();
 
 const router = express.Router();
-app.use(router);
 
 const { Pool } = require("pg");
 const bodyParser = require("body-parser");
