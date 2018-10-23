90581136e4447240570ef657bb98627152be1520
Daleel Hagy <daleel.n.hagy@gmail.com>
2018-10-23 17:58:19 +0100

    changing space around the volunteer button

--- frontend/src/Components/Event.js
+++ frontend/src/Components/Event.js
@@ -5,25 +5,17 @@ import Popup from "reactjs-popup";
 import MyForm from "./MyForm.js";
 
 const Event = props => {
-    return (
-        <div className="event">
+    return <div className="event">
             <h1>{props.name}</h1>
             <p>Description: {props.description}</p>
             <div>Date: {props.date}</div>
             <div>Floaters: {props.floaters}</div>
-            <Popup
-                trigger={
-                    <button type="button" class="btn btn-outline-primary">
+            <Popup trigger={<button type="button" class="btn btn-outline-primary mt-4">
                         volunteer
-                    </button>
-                }
-                position="right center"
-                modal
-            >
+                    </button>} position="right center" modal>
                 <MyForm />
             </Popup>
-        </div>
-    );
+        </div>;
 };
 
 export default Event;
