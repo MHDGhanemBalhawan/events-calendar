6d1cefd1f068dbb2f34c4eeb02442be10cbf23ab
Steve <mhdghanembalhawan@hotmail.com>
2018-10-30 19:47:22 +0000

    Fixed body-parser and made the code more descriptive

--- backend/route/router.js
+++ backend/route/router.js
@@ -9,9 +9,9 @@ var connectionString =
 
 const pool = new Pool({ connectionString: connectionString });
 
-pool.connect(function(err, poolClient) {
-  if (err) {
-    console.log("Failed to connect to database: " + err);
+pool.connect(function(connectionError, poolClient) {
+  if (connectionError) {
+    console.log("Failed to connect to database: " + connectionError);
   } else {
     console.log("Connected to database");
   }
@@ -19,228 +19,234 @@ pool.connect(function(err, poolClient) {
 
 // events router
 
-router.get("/events", (req, res) => {
+router.get("/events", (httpRequest, httpResponse) => {
   pool.query(
     "SELECT event_id, lesson, event_date as date, description FROM events_tbl",
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        res.status(400).send(err);
+    (dbError, dbResult) => {
+      if (dbError) {
+        httpResponse.status(400).send(dbError);
       } else {
-        res.status(200).send(result.rows);
+        httpResponse.status(200).send(dbResult.rows);
       }
     }
   );
 });
 
-router.get("/events/:id", function(req, res) {
-  const id = req.params.id;
+router.get("/events/:id", function(httpRequest, httpResponse) {
+  const id = httpRequest.params.id;
   const query = {
     text: `SELECT event_id, lesson, event_date as date, description FROM events_tbl WHERE events_tbl.event_id = ${id};`
   };
-  pool.query(query, (err, result) => {
-    if (err) {
-      res.status(500).send(err);
+  pool.query(query, (dbError, dbResult) => {
+    if (dbError) {
+      httpResponse.status(500).send(dbError);
     } else {
-      res.status(200).send(result.rows[0]);
+      httpResponse.status(200).send(dbResult.rows[0]);
     }
   });
 });
 
-router.post("/events", (req, res) => {
-  console.log(req.body);
+router.post("/events", (httpRequest, httpResponse) => {
   const query = {
     text:
       "INSERT INTO events_tbl(lesson, event_date, description) VALUES($1, $2, $3)",
-    values: [req.body.lesson, req.body.event_date, req.body.description]
+    values: [
+      httpRequest.body.lesson,
+      httpRequest.body.event_date,
+      httpRequest.body.description
+    ]
   };
   //callback;
-  pool.query(query, (err, response) => {
-    if (err) {
-      res.status(500).send(err);
+  pool.query(query, (dbError, response) => {
+    if (dbError) {
+      httpResponse.status(500).send(dbError);
     } else {
-      res.status(200).send("OK");
+      httpResponse.status(200).send("OK");
     }
   });
 });
-router.delete("/events/:id", (req, res) => {
-  const id = req.params.id;
+router.delete("/events/:id", (httpRequest, httpResponse) => {
+  const id = httpRequest.params.id;
   const query = {
     text: `DELETE FROM public.events_tbl WHERE public.events_tbl.event_id = ${id};`
   };
-  pool.query(query, (err, response) => {
-    if (err) {
-      res.status(500).send(err);
+  pool.query(query, (dbError, dbResult) => {
+    if (dbError) {
+      httpResponse.status(500).send(dbError);
     } else {
-      res.status(200).send("OK");
+      httpResponse.status(200).send("OK");
     }
   });
 });
 
-router.put("/events/:id", (req, res) => {
-  const id = req.params.id;
+router.put("/events/:id", (httpRequest, httpResponse) => {
+  const id = httpRequest.params.id;
   const query = {
     text:
       "UPDATE public.events_tbl SET lesson = $1, event_date = $2, description = $3 WHERE event_id = $4;",
-    values: [req.body.lesson, req.body.event_date, req.body.description, id]
+    values: [
+      httpRequest.body.lesson,
+      httpRequest.body.event_date,
+      httpRequest.body.description,
+      id
+    ]
   };
-  pool.query(query, (err, response) => {
-    if (err) {
-      res.status(500).send(err);
+  pool.query(query, (dbError, dbResult) => {
+    if (dbError) {
+      httpResponse.status(500).send(dbError);
     } else {
-      res.status(200).send("OK");
+      httpResponse.status(200).send("OK");
     }
   });
 });
 
 // mentors router
 
-router.get("/mentors", (req, res) => {
-  pool.query("SELECT * FROM floaters_tbl", (err, result) => {
-    if (err) {
-      console.log(err);
-      res.status(400).send(err);
+router.get("/mentors", (httpRequest, httpResponse) => {
+  pool.query("SELECT * FROM floaters_tbl", (dbError, dbResult) => {
+    if (dbError) {
+      console.log(dbError);
+      httpResponse.status(400).send(dbError);
     } else {
-      res.status(200).send(result.rows);
+      httpResponse.status(200).send(dbResult.rows);
     }
   });
 });
 
-router.get("/mentors/:id", function(req, res) {
-  const id = req.params.id;
+router.get("/mentors/:id", (httpRequest, httpResponse) => {
+  const id = httpRequest.params.id;
   const query = {
     text: `SELECT * FROM floaters_tbl WHERE floaters_tbl.floater_id = ${id};`
   };
-  pool.query(query, (err, result) => {
-    if (err) {
-      res.status(500).send(err);
+  pool.query(query, (dbError, dbResult) => {
+    if (dbError) {
+      httpResponse.status(500).send(dbError);
     } else {
-      res.status(200).send(result.rows[0]);
+      httpResponse.status(200).send(dbResult.rows[0]);
     }
   });
 });
-router.put("/mentors/:id", (req, res) => {
-  const id = req.params.id;
-
+router.put("/mentors/:id", (httpRequest, httpResponse) => {
+  const id = httpRequest.params.id;
   const query = {
     text: ` UPDATE floaters_tbl SET floater_fname = '${
-      req.body.floater_fname
-    }', floater_surname = '${req.body.floater_surname}', floater_email = '${
-      req.body.floater_email
+      httpRequest.body.floater_fname
+    }', floater_surname = '${
+      httpRequest.body.floater_surname
+    }', floater_email = '${
+      httpRequest.body.floater_email
     }' WHERE floater_id = ${id};`
   };
-  pool.query(query, (err, response) => {
-    if (err) {
-      res.status(500).send(err);
+  pool.query(query, (dbError, dbResult) => {
+    if (dbError) {
+      httpResponse.status(500).send(dbError);
     } else {
-      res.status(200).send("OK");
+      httpResponse.status(200).send("OK");
     }
   });
 });
-router.post("/mentors", (req, res) => {
+router.post("/mentors", (httpRequest, httpResponse) => {
   const query = {
     text:
       "INSERT INTO public.floaters_tbl(floater_fname, floater_surname, floater_email) VALUES($1, $2, $3)",
     values: [
-      req.body.floater_fname,
-      req.body.floater_surname,
-      req.body.floater_email
+      httpRequest.body.floater_fname,
+      httpRequest.body.floater_surname,
+      httpRequest.body.floater_email
     ]
   };
   //callback;
-  pool.query(query, (err, response) => {
-    if (err) {
-      res.status(500).send(err);
+  pool.query(query, (dbError, dbResult) => {
+    if (dbError) {
+      httpResponse.status(500).send(dbError);
     } else {
-      res.status(200).send("OK");
+      httpResponse.status(200).send("OK");
     }
   });
 });
-router.delete("/mentors/:id", (req, res) => {
-  const id = req.params.id;
+router.delete("/mentors/:id", (httpRequest, httpResponse) => {
+  const id = httpRequest.params.id;
   const query = {
     text: `DELETE FROM public.floaters_tbl WHERE public.floaters_tbl.floater_id = ${id};`
   };
-  pool.query(query, (err, response) => {
-    if (err) {
-      res.status(500).send(err);
+  pool.query(query, (dbError, dbResult) => {
+    if (dbError) {
+      httpResponse.status(500).send(dbError);
     } else {
-      res.status(200).send("OK");
+      httpResponse.status(200).send("OK");
     }
   });
 });
-
 // events-floaters router
-router.get("/events-floaters", (req, res) => {
+router.get("/events-floaters", (httpRequest, httpResponse) => {
   pool.query(
     "SELECT floaters_events_id, event_id, floater_id FROM floaters_events_tbl;",
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        res.status(400).send(err);
+    (dbError, dbResult) => {
+      if (dbError) {
+        console.log(dbError);
+        httpResponse.status(400).send(dbError);
       } else {
-        res.status(200).send(result.rows);
+        httpResponse.status(200).send(dbResult.rows);
       }
     }
   );
 });
-router.get("/events-floaters/:id", function(req, res) {
-  const id = req.params.id;
+router.get("/events-floaters/:id", (httpRequest, httpResponse) => {
+  const id = httpRequest.params.id;
   const query = {
     text: `SELECT floaters_events_id, event_id, floater_id FROM public.floaters_events_tbl WHERE floaters_events_tbl.floaters_events_id = ${id};`
   };
-  pool.query(query, (err, result) => {
-    if (err) {
-      res.status(500).send(err);
+  pool.query(query, (dbError, dbResult) => {
+    if (dbError) {
+      httpResponse.status(500).send(dbError);
     } else {
-      res.status(200).send(result.rows[0]);
+      httpResponse.status(200).send(dbResult.rows[0]);
     }
   });
 });
-
-router.post("/events-floaters", (req, res) => {
+router.post("/events-floaters", (httpRequest, httpResponse) => {
   const query = {
     text:
       "INSERT INTO floaters_events_tbl(event_id, floater_id) VALUES($1, $2)",
-    values: [req.body.event_id, req.body.floater_id]
+    values: [httpRequest.body.event_id, httpRequest.body.floater_id]
   };
   //callback;
-  pool.query(query, (err, response) => {
-    if (err) {
-      res.status(500).send(err);
+  pool.query(query, (dbError, dbResult) => {
+    if (dbError) {
+      httpResponse.status(500).send(dbError);
     } else {
-      res.status(200).send("OK");
+      httpResponse.status(200).send("OK");
     }
   });
 });
-router.delete("/events-floaters/:id", (req, res) => {
-  const id = req.params.id;
+router.delete("/events-floaters/:id", (httpRequest, httpResponse) => {
+  const id = httpRequest.params.id;
   const query = {
     text: `DELETE FROM public.floaters_events_tbl WHERE floaters_events_tbl.floaters_events_id = ${id};`
   };
-  pool.query(query, (err, response) => {
-    if (err) {
-      res.status(500).send(err);
+  pool.query(query, (dbError, dbResult) => {
+    if (dbError) {
+      httpResponse.status(500).send(dbError);
     } else {
-      res.status(200).send("OK");
+      httpResponse.status(200).send("OK");
     }
   });
 });
-
-router.put("/events-floaters/:id", (req, res) => {
-  const id = req.params.id;
+router.put("/events-floaters/:id", (httpRequest, httpResponse) => {
+  const id = httpRequest.params.id;
   const query = {
     text: `UPDATE public.floaters_events_tbl SET event_id = '${
-      req.body.event_id
-    }', floater_id = '${req.body.floater_id}' WHERE floaters_events_id = ${id};`
+      httpRequest.body.event_id
+    }', floater_id = '${
+      httpRequest.body.floater_id
+    }' WHERE floaters_events_id = ${id};`
   };
-  pool.query(query, (err, response) => {
-    if (err) {
-      res.status(500).send(err);
+  pool.query(query, (dbError, dbResult) => {
+    if (dbError) {
+      httpResponse.status(500).send(dbError);
     } else {
-      res.status(200).send("OK");
+      httpResponse.status(200).send("OK");
     }
   });
 });
-
 module.exports = router;
