c70e9edcfaf06ac0aeb860501aca2498c8f726f4
Behnaz <behnaz.pourafshari@gmail.com>
2018-11-14 08:45:47 +0000

    I add some CSS to main page and admin main page

--- frontend/src/Components/Public/MainPage.js
+++ frontend/src/Components/Public/MainPage.js
@@ -1,15 +1,27 @@
 import React from "react";
+import "../../Style/MainPage.css"
 const mainPage = () => (
-    <div className="container mt-5">
-        <h1 className="mb-3">CYF Events Calendar</h1>
-        <ul className="nav flex-column">
-            <li className="nav-item">
-                <a href="/admin">Admin</a>
-            </li>
-            <li className="nav-item">
-                <a href="/events">Events</a>
-            </li>
-        </ul>
+    <div className="container">
+        <div className="row justify-content-md-center title">
+            <div className="col-md-auto">
+                <h1 className="mb-3 mt-5">Events Calendar</h1>
+            </div>
+        </div>
+        <div>
+            <ul className="nav flex-column">
+                    <li className="nav-item text-center shadow-lg p-3 mb-5 rounded">
+                        <h2>
+                            <a href="/admin">Admin</a>
+                        </h2>
+                    </li>
+                    <li className="nav-item text-center shadow-lg p-3 mb-5  rounded">
+                        <h2>
+                            <a href="/events">Events</a>
+                        </h2>
+                    </li>
+
+            </ul>
+        </div>
     </div>
 );
 
