e7b60027c774611124b180995eade8dd2a361b67
Behnaz <behnaz.pourafshari@gmail.com>
2018-11-19 21:09:55 +0000

    WIP to improve main page

--- frontend/src/Components/Public/MainPage.js
+++ frontend/src/Components/Public/MainPage.js
@@ -1,26 +1,30 @@
 import React from "react";
 import "../../Style/MainPage.css"
 const mainPage = () => (
-    <div className="container">
+    <div className="containe container">
         <div className="row justify-content-md-center title">
             <div className="col-md-auto">
-                <h1 className="mb-3 mt-5">Events Calendar</h1>
+                <h1 className="event-title">Events Calendar</h1>
             </div>
         </div>
         <div>
-            <ul className="nav flex-column">
-                    <li className="nav-item text-center shadow-lg p-3 mb-5 rounded">
-                        <h2>
-                            <a href="/admin">Admin</a>
-                        </h2>
-                    </li>
-                    <li className="nav-item text-center shadow-lg p-3 mb-5  rounded">
-                        <h2>
-                            <a href="/events">Events</a>
-                        </h2>
-                    </li>
+            <ul className="nav justify-content-between ">
+                <li className="nav-item text-center shadow-lg p-3 mb-5 rounded">
+                    <h2>
+                        <a href="/admin">Admin</a>
+                    </h2>
+                </li>
+                <li className="nav-item text-center shadow-lg p-3 mb-5  rounded">
+                    <h2>
+                        <a href="/events">Events</a>
+                    </h2>
+                </li>
+
+
 
             </ul>
+
+
         </div>
     </div>
 );
