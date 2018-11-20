ee42731628dcaa7222477d4c29df039f61d938ed
Daleel Hagy <daleel.n.hagy@gmail.com>
2018-11-20 22:19:37 +0000

    new nav bar and fixing buttons

--- frontend/src/Components/Admin/Floaters/Floaters.js
+++ frontend/src/Components/Admin/Floaters/Floaters.js
@@ -32,20 +32,31 @@ export default class Floaters extends React.Component {
     };
 
     render() {
-        return (
-            <div className="container mt-2">
-                <Message
-                    show={this.state.message}
-                    status="success"
-                    message="New floater is deleted"
-                />
-                <div className=" mb-4 mt-4">
-                    <h1 className="h3 mb-4">Code Your Future Floaters</h1>
+        return <div className="container mt-2">
+                <Message show={this.state.message} status="success" message="New floater is deleted" />
+
+                <div className=" mb-4 mt-4 sameRowBtn container">
+                    <h1 className="h3 mb-4 myHeader">
+                        Code Your Future Floaters
+                    </h1>
+
+                    <a href="/admin">
+                        <button className="btn btn-outline-primary mr-4 mb-2 sideButton">
+                            Back
+                        </button>
+                    </a>
+                        <a href="/admin/floaters/add">
+                            {" "}
+                            <button className="btn btn-outline-primary mr-4 mb-2 sideButton">
+                                Add Floaters
+                            </button>
+                        </a>
                 </div>
-                <table className="table table-striped table-hover ">
+                <table className="table table-striped table-dark">
                     <tbody>
                         <tr>
-                            <th scope="col">Floater id</th>
+                            {/* <th scope="col">Floater id</th> */}
+                            <th />
                             <th scope="col">First Name </th>
                             <th scope="col">Surname </th>
                             <th scope="col">Email</th>
@@ -53,65 +64,30 @@ export default class Floaters extends React.Component {
                             <th scope="col">Delete</th>
                         </tr>
                         {this.state.mentors.map(floater => {
-                            return (
-                                <tr key={floater.floater_id}>
-                                    <th scope="row">{floater.floater_id}</th>
+                            return <tr key={floater.floater_id}>
+                                    <td />
+                                    {/* <th scope="row">{floater.floater_id}</th> */}
                                     <td>{floater.floater_fname}</td>
                                     <td>{floater.floater_surname}</td>
                                     <td>{floater.floater_email}</td>
                                     <td>
-                                        <Popup
-                                            trigger={
-                                                <button
-                                                    type="button"
-                                                    className="btn btn-warning"
-                                                >
+                                        <Popup trigger={<button type="button" className="btn btn-link text-warning linkEdit">
                                                     Edit
-                                                </button>
-                                            }
-                                            position="right center"
-                                            modal
-                                        >
-                                            <EditFloater
-                                                floater_fname={
-                                                    floater.floater_fname 
-                                                }
-                                                floater_surname={
-                                                    floater.floater_surname
-                                                }
-                                                floater_email={
-                                                    floater.floater_email
-                                                }
-                                                floater_id={floater.floater_id}
-                                            />
+                                                </button>} position="right center" modal>
+                                            <EditFloater floater_fname={floater.floater_fname} floater_surname={floater.floater_surname} floater_email={floater.floater_email} floater_id={floater.floater_id} />
                                         </Popup>
                                     </td>
                                     <td>
-                                        <button
-                                            type="button"
-                                            className="btn btn-danger"
-                                            onClick={() => {
-                                                this._deleteFloater(
-                                                    floater.floater_id
-                                                );
-                                            }}
-                                        >
+                                        <button type="button" className="btn btn-link text-danger linkDelete" onClick={() => {
+                                                this._deleteFloater(floater.floater_id);
+                                            }}>
                                             Delete
                                         </button>
                                     </td>
-                                </tr>
-                            );
+                                </tr>;
                         })}
                     </tbody>
                 </table>
-
-                <button className="btn btn-outline-primary mr-4 mb-2 mt-4">
-                    <a href="/admin">Back</a>
-                </button>
-                <button className="btn btn-outline-primary mr-4 mb-2 mt-4">
-                    <a href="/admin/floaters/add">Add Floaters</a>
-                </button>
-            </div>
-        );
+            </div>;
     }
 }
