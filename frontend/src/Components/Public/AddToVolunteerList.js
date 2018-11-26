d1cf1508123e99895336d6437686a2b6a3e7eeed
Daleel <36134384+daleel1@users.noreply.github.com>
2018-11-26 03:17:24 +0000

    removing comments

--- frontend/src/Components/Public/AddToVolunteerList.js
+++ frontend/src/Components/Public/AddToVolunteerList.js
@@ -1,7 +1,5 @@
 import React from "react";
 import Message from "../Message/Message";
-// import Popup from "reactjs-popup";
-// import VolunteerForm from "../Admin/Floaters/FloaterToVolunteer";
 
 export default class AddToVolunteerList extends React.Component {
     state = { message: false };
