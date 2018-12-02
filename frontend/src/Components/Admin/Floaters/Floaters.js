d59636a036c965f441626898546f6c072ac89f37
Daleel Hagy <daleel.n.hagy@gmail.com>
2018-12-02 21:19:30 +0000

    adding nav bar component on Floaters Page

--- frontend/src/Components/Admin/Floaters/Floaters.js
+++ frontend/src/Components/Admin/Floaters/Floaters.js
@@ -5,6 +5,8 @@ import Popup from "reactjs-popup";
 import EditFloater from "./EditFloater.js";
 import "../../../Style/Events.css";
 import FloaterForm from "./FloaterForm";
+import NavBar from "../../NavBar";
+
 
 export default class Floaters extends React.Component {
     state = {
@@ -34,37 +36,28 @@ export default class Floaters extends React.Component {
     };
 
     render() {
-        return (
-            <div className="container mt-2">
-                <div className="dummyDiv" />
-                <div className="fixed-top card headerEvents  mt=0">
-                    <div className=" mb-3 mt-3 sameRowBtn container">
-                        <h1 className="myHeader ml-5 ">
-                            Code Your Future Floaters
-                        </h1>
-                        <a href="/admin">
-                            <button className="btn btn-outline-primary ml-2 mb-2 sideButton">
-                                Back
-                            </button>
-                        </a>
-                        <Popup
-                            trigger={
-                                <button className="btn btn-outline-primary mb-2 ml-2 sideButton mr-5 ">
-                                    Add a New Floater
-                                </button>
-                            }
-                            position="right center"
-                            modal
-                        >
-                            <FloaterForm />
-                        </Popup>
-                    </div>
-                </div>
-                <Message
-                    show={this.state.message}
-                    status="success"
-                    message="New floater is deleted"
-                />
+        return <div className="container mt-2">
+                {/* <div className="dummyDiv" />
+                <div className="fixed-top card headerEvents  mt=0"> */}
+                <NavBar>
+                    {/* <div className=" mb-3 mt-3 sameRowBtn container"> */}
+                    <h1 className="myHeader ml-5">
+                        Code Your Future Floaters
+                    </h1>
+                    <Popup trigger={<button className="btn btn-outline-primary mb-2 ml-2 sideButton mr-5 ">
+                                Add a New Floater
+                            </button>} position="right center" modal>
+                        <FloaterForm />
+                    </Popup>
+                <a href="/admin">
+                    <button className="btn btn-outline-primary ml-2 mb-2 sideButton">
+                        Back
+                        </button>
+                </a>
+                    {/* </div> */}
+                </NavBar>
+                {/* </div> */}
+                <Message show={this.state.message} status="success" message="New floater is deleted" />
 
                 <table className="table table-striped table-dark">
                     <tbody>
@@ -78,58 +71,29 @@ export default class Floaters extends React.Component {
                             <th scope="col">Delete</th>
                         </tr>
                         {this.state.mentors.map(floater => {
-                            return (
-                                <tr key={floater.floater_id}>
+                            return <tr key={floater.floater_id}>
                                     <td />
                                     <td>{floater.floater_fname}</td>
                                     <td>{floater.floater_surname}</td>
                                     <td>{floater.floater_email}</td>
                                     <td>
-                                        <Popup
-                                            trigger={
-                                                <button
-                                                    type="button"
-                                                    className="btn btn-link text-warning linkEdit"
-                                                >
+                                        <Popup trigger={<button type="button" className="btn btn-link text-warning linkEdit">
                                                     Edit
-                                                </button>
-                                            }
-                                            position="right center"
-                                            modal
-                                        >
-                                            <EditFloater
-                                                floater_fname={
-                                                    floater.floater_fname
-                                                }
-                                                floater_surname={
-                                                    floater.floater_surname
-                                                }
-                                                floater_email={
-                                                    floater.floater_email
-                                                }
-                                                floater_id={floater.floater_id}
-                                            />
+                                                </button>} position="right center" modal>
+                                            <EditFloater floater_fname={floater.floater_fname} floater_surname={floater.floater_surname} floater_email={floater.floater_email} floater_id={floater.floater_id} />
                                         </Popup>
                                     </td>
                                     <td>
-                                        <button
-                                            type="button"
-                                            className="btn btn-link text-danger linkDelete"
-                                            onClick={() => {
-                                                this._deleteFloater(
-                                                    floater.floater_id
-                                                );
-                                            }}
-                                        >
+                                        <button type="button" className="btn btn-link text-danger linkDelete" onClick={() => {
+                                                this._deleteFloater(floater.floater_id);
+                                            }}>
                                             Delete
                                         </button>
                                     </td>
-                                </tr>
-                            );
+                                </tr>;
                         })}
                     </tbody>
                 </table>
-            </div>
-        );
+            </div>;
     }
 }
