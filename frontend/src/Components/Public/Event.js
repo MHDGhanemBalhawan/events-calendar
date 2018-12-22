999f682c3a4b0f7068b841a3b666f5313600d396
Daleel Hagy <daleel.n.hagy@gmail.com>
2018-12-22 03:50:32 +0000

    restayling events

--- frontend/src/Components/Public/Event.js
+++ frontend/src/Components/Public/Event.js
@@ -4,13 +4,16 @@ import "../../Style/Event.css";
 import "../../Style/Events.css";
 
 const Event = props => {
-    return <span>
-        
-        <h1 className="float-right">{props.name}</h1>
-        <h1 >{moment(props.date).format("Do MMMM  YYYY")}</h1>
+    return (
+        <span>
+            <h4 className="float-right font-italic">
+                {moment(props.date).format("Do MMMM  YYYY")}
+            </h4>
+            <h1>{props.name}</h1>
             <p>{props.description}</p>
             <div>Location: TicketMaster offices</div>
-        </span>;
+        </span>
+    );
 };
 
 export default Event;
