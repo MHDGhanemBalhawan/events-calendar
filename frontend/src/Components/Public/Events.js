7ea1a328695854a7434006d17b0616fff1865512
Daleel Hagy <daleel.n.hagy@gmail.com>
2019-01-20 16:22:25 +0000

    editing UI for events

--- frontend/src/Components/Public/Events.js
+++ frontend/src/Components/Public/Events.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Event from "./Event.js";
 import "../../Style/Event.css";
-import FloatersOfEvents from "../Admin/Floaters/FloatersOfEvent.js";
-import Popup from "reactjs-popup";
-import VolunteerForm from "../Admin/Floaters/FloaterToVolunteer";
+// import FloatersOfEvents from "../Admin/Floaters/FloatersOfEvent.js";
 import NavBar from "../NavBar";
+import "../../Style/Events.css";
+
 
 const Events = props => {
     return (
@@ -14,42 +14,20 @@ const Events = props => {
             </NavBar>
             {props.events.map(function(event, i) {
                 return (
-                    <div className="event bg-light text-dark" key={i}>
-                        <a href={`/event/${event.event_id}`} alt={event.lesson}>
-                            <Event
-                                key={i}
-                                name={event.lesson}
-                                date={
-                                    event.date // description={event.description}
-                                }
-                            />
-
-                            <FloatersOfEvents id={event.event_id} />
-                        </a>
-
-                        <Popup
-                            trigger={
-                                <button
-                                    type="button"
-                                    className="btn btn-outline-primary mr-4 mb-2 mt-4"
-                                >
-                                    volunteer
-                                </button>
+                    // <div
+                    //     className=" bg-light text-dark border-bottom border-right"
+                    //     key={i}
+                    // >
+                        <Event
+                            key={i}
+                            name={event.lesson}
+                            event={event}
+                            date={
+                                event.date // description={event.description}
                             }
-                            position="right center"
-                            modal
-                        >
-                            <VolunteerForm event_id={event.event_id} />
-                        </Popup>
-
-                        <a
-                            className="btn btn-link mr-4 mb-2 mt-4"
-                            href={`/event/${event.event_id}`}
-                            alt={event.lesson}
-                        >
-                            Read more
-                        </a>
-                    </div>
+                        />
+                        //  <FloatersOfEvents id={event.event_id} />
+                    // </div>
                 );
             })}
         </div>
