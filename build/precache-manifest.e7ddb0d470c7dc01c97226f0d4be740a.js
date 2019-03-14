384638db0dbb21918278daa19a37ec9b6d84e726
Steve <mhdghanembalhawan@hotmail.com>
2019-03-14 22:49:22 +0000

    moved build to main dir

---
+++ build/precache-manifest.e7ddb0d470c7dc01c97226f0d4be740a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "01ea829f8f58b85821e7",
+    "url": "/static/js/main.01ea829f.chunk.js"
+  },
+  {
+    "revision": "f30ab5f0e2f9b51c3466",
+    "url": "/static/js/1.f30ab5f0.chunk.js"
+  },
+  {
+    "revision": "01ea829f8f58b85821e7",
+    "url": "/static/css/main.603ab5cc.chunk.css"
+  },
+  {
+    "revision": "efec102a9f371b554cc3c43c8b8c3a36",
+    "url": "/index.html"
+  }
+];
